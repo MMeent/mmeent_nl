e98ceae8bc579e2ef370a5cf6b615663455b6bb1
mattashii <boekewurm@gmail.com>
2017-02-14 12:13:51 +0100

    update things

--- www/views.py
+++ www/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views import View
+from django.views.generic import TemplateView
 
 
-class IndexView(View):
+class IndexView(TemplateView):
     template_name = 'base.html'
-
